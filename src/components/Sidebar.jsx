91fb88aabe73205069fefae4a243d4ff462b130b
André Nicoletti <andregnicoletti@gmail.com>
2025-04-09 00:46:24 -0300

    lesson: component sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react'
 
 export function Sidebar() {
 
@@ -8,14 +9,18 @@ export function Sidebar() {
                 className={styles.cover}
                 src='https://images.unsplash.com/photo-1743083931860-05b751559acb?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
             />
-            
+
             <div className={styles.profile}>
+
+                <img className={styles.avatar} src='https://github.com/andregnicoletti.png' />
+
                 <strong>André Nicoletti</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href='#'>
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
